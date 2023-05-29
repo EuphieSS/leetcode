0044fbcfc9965ff02c7aa20cafdceecc9512aa85
Samma Su <109315320+EuphieSS@users.noreply.github.com>
2023-05-29 19:37:14 -0400

    Time: 87 ms, Memory: 44.7 MB - LeetHub

---
+++ find-pivot-index/find-pivot-index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+  const totalSum = nums.reduce(
+    (accu, currentValue) => accu + currentValue, 0
+  );
+  
+  let leftSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (leftSum === totalSum - nums[i] - leftSum) {
+      return i;
+    } else {
+      leftSum += nums[i];
+    }
+  }
+  
+  return -1;
+};
