1cd7397da58506306cf06e0bbf5399accc5c6802
Samma Su <109315320+EuphieSS@users.noreply.github.com>
2023-05-30 15:51:41 -0400

    Time: 173 ms (25.64%), Space: 52.2 MB (79.33%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  nums.sort((a, b) => a - b);
+  
+  let i = 0;
+  let j = nums.length - 1;
+  while (i < j) {
+    if (nums[i] === nums[i + 1] || nums[j] === nums[j - 1]) {
+      return true;
+    }
+    i++;
+    j--;
+  }
+  
+  return false;
+};
