632e611f4da5b9b6f82f80a34edf1cfe3ede8efb
Samma Su <109315320+EuphieSS@users.noreply.github.com>
2023-07-08 16:32:49 -0400

    Time: 60 ms (94.76%), Space: 43.9 MB (85.59%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.js
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.js
@@ -12,19 +12,6 @@ var shortestToChar = function(s, c) {
       cIndices.push(i);
     }
   }
-  
-  // let i = 0;
-  // let j = 0;
-  // while (i < s.length) {
-  //   output.push(cIndices[j] - i);
-  //   if (i === cIndices[j]) {
-  //     j++;
-  //   }
-  //   i++
-  // }
-  
-  // now cIndicies = [3, 5, 6, 11]
-  
   cIndices.push(99999)
   
   let i = 0;
@@ -35,18 +22,14 @@ var shortestToChar = function(s, c) {
     }
     else if (i === cIndices[j]) {
       output.push(0);
-      //console.log(`current j is: ${j}, current i is ${i}`)
       j++;
     } else {
-      //console.log(`current j is: ${j}, current i is ${i}`)
-      //console.log(cIndices[j] - i);
-      //console.log(cIndices[j-1] - i);
+
       let smallest = Math.min(Math.abs(cIndices[j] - i), Math.abs(cIndices[j-1] -i));
       output.push(smallest)
     }
     i++;
   }
-  
-  console.log(cIndices);
+
   return output;
 };
