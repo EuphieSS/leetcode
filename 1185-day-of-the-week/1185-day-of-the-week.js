52b53b6ba951898e879f2ba851140b24b1670165
Samma Su <109315320+EuphieSS@users.noreply.github.com>
2023-07-27 20:32:46 -0400

    Time: 43 ms (100.00%), Space: 42.1 MB (77.85%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function(day, month, year) {
+  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const date = new Date(`${year}-${month}-${day}`);
+  const dayOfWeek = date.getDay();
+  return dayNames[dayOfWeek];  
+};
