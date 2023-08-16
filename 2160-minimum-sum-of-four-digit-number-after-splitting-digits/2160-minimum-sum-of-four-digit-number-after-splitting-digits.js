4650e921439a514a4fc292f8ec706e96a5ab85b1
Samma Su <109315320+EuphieSS@users.noreply.github.com>
2023-08-15 21:07:40 -0400

    Time: 50 ms (86.92%), Space: 42.6 MB (9.03%) - LeetHub

---
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var minimumSum = function(num) {
+  const numsArr = Array.from(String(num));
+  numsArr.sort((a, b) => a - b);
+  const pairs = [];
+  let i = 0;
+  let j = numsArr.length - 1;
+  while (i < j) {
+    pairs.push(Number(numsArr[i] + numsArr[j]));
+    i++;
+    j--;
+  }
+  const sum = pairs.reduce((acc, cV) => acc + cV, 0);
+  return sum;
+};
